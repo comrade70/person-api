b3ebc6ce6d343a175b9a4ed9286bf7bf13763aa0
comrade70 <ademolajoseph88@gmail.com>
2023-09-15 03:46:07 +0100

    new commit

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
 from .models import Person
+import re
 
 class PersonSerializer(serializers.ModelSerializer):
-    class Meta:
+     def validate_name(self, value):
+        #regex pattern to validate name
+        name_pattern = re.compile(r'^[A-Za-z\s]+ [A-Za-z\s]+$')
+        if not name_pattern.match(value):
+            raise serializers.ValidationError("Please provide full names in letters only")
+        return value
+
+     class Meta:
         model = Person
         fields = ['id', 'name']
