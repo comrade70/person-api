a1aceedcf28c4b4c83c6d95abb95959f3b9a16ba
comrade70 <ademolajoseph88@gmail.com>
2023-09-15 14:03:34 +0100

    new commit

--- api/serializers.py
+++ api/serializers.py
@@ -3,11 +3,14 @@ from .models import Person
 import re
 
 class PersonSerializer(serializers.ModelSerializer):
+
+     id = serializers.IntegerField(read_only=True)
+     name = serializers.CharField(max_length=100)
      def validate_name(self, value):
         #regex pattern to validate name
-        name_pattern = re.compile(r'^[A-Za-z\s]+ [A-Za-z\s]+$')
+        name_pattern = re.compile(r'^[A-Za-z\s]+$') #this accomodate single or double names
         if not name_pattern.match(value):
-            raise serializers.ValidationError("Please provide full names in letters only")
+            raise serializers.ValidationError("Please provide names in letters only")
         return value
 
      class Meta:
