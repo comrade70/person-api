5f68960199d9079db986f73399981a69e480a066
comrade70 <ademolajoseph88@gmail.com>
2023-09-15 23:55:20 +0100

    views.py updated

--- api/views.py
+++ api/views.py
@@ -3,14 +3,14 @@ from .models import Person
 from .serializers import PersonSerializer
 
 class PersonListCreateView(generics.ListCreateAPIView):
-    queryset = Person.objects.all()
     serializer_class = PersonSerializer
     def get_queryset(self):
         queryset = Person.objects.all()
-        name = self.request.query_params.get('name, None')
+        name = self.request.query_params.get('name', None)
         if name is not None:
             queryset = queryset.filter(name=name)
         return queryset
+
 class PersonRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
